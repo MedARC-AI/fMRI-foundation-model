092cd113a1fb57fb8392d91dabf783d4cc05354b
Paul Scotti <paulscotti@ip-10-0-236-227.us-west-2.compute.internal>
2024-03-07 17:56:42 +0000

    MNI changes and new dataloader

--- setup.sh
+++ setup.sh
@@ -5,7 +5,14 @@ set -e
 
 pip install --upgrade pip
 
-python3.11 -m venv fmri
-source fmri/bin/activate
+python3.10 -m venv found
+source found/bin/activate
 
-pip install numpy matplotlib jupyter jupyterlab_nvdashboard jupyterlab scipy tqdm scikit-learn scikit-image accelerate webdataset pandas matplotlib einops ftfy regex h5py torchvision torch==2.1.0 transformers xformers torchmetrics deepspeed wandb nilearn nibabel boto3 open_clip_torch kornia
+pip install numpy matplotlib jupyter jupyterlab_nvdashboard jupyterlab scipy tqdm scikit-learn scikit-image accelerate webdataset pandas matplotlib einops ftfy regex h5py torchvision torch==2.2.0 transformers xformers torchmetrics deepspeed wandb nilearn nibabel boto3 open_clip_torch kornia omegaconf decord smart-open ffmpeg-python opencv-python==4.6.0.66
+
+# If you are using Stability HPC then do this (may need to fix the absolute path to .whl):
+pip install fMRI-foundation-model/torchdata_whl/torchdata-0.5.1+cb9ed24-cp310-cp310-linux_x86_64.whl
+pip install s3fs -U
+
+# If you are NOT using Stability HPC then do this:
+# pip install torchdata==0.6.1 --no-deps
